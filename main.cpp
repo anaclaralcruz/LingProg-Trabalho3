b4aaa4205826151da423410a778ca13949eec228
Ana Clara Cruz <anaclaralcruz@poli.ufrj.br>
2021-05-17 00:27:23 -0300

    Implement funcionalities 3, 4 and 5

--- main.cpp
+++ main.cpp
@@ -48,6 +48,51 @@ int main (){
         cout << "Filme adicionado com sucesso!!" << endl ;
     }
 
+    else if (opcaoMenu == 3){
+        bool novoFilme = true;
+        Filme filme ;
+        vector <Filme> entrada ;
+        char in ;
+
+        while (novoFilme){
+            cout << "Escreva [Nome do filme] ENTER [Produtora] ENTER [nota] ENTER :\t" ;
+            cin >> filme ;
+            entrada.push_back(filme);
+        
+            cout << "Adicionar outro filme? (s/n) " ;
+            cin >> in;
+            cin.ignore();
+            if (in == 'n')
+                novoFilme = false;
+        }
+        catalogo += entrada;
+    }
+
+    else if (opcaoMenu == 4){
+        string nomeDoFilme ;
+
+        cout << "Escreva o nome do filme que deseja remover: " ;
+        cin >> nomeDoFilme ;
+        
+        Filme* filme = catalogo(nomeDoFilme);
+
+        catalogo -= *filme ;
+        cout << "Filme \"" << filme->getNome() << "\" removido com sucesso!!" << endl;
+    }
+
+    else if (opcaoMenu == 5){
+        string nomeDoFilme ;
+
+        cout << "Escreva o nome do filme que esta procurando: " ;
+        cin >> nomeDoFilme ;
+        
+        Filme* filme = catalogo(nomeDoFilme);
+
+        if (!filme) cout << "FILME NAO ENCONTRADO!" << endl;
+        else cout << *filme << endl;
+    }
+   
+
     else {
         cout << "ENTRADA INVALIDA escolha um numero de 1 a 8 !!" << endl;
         main ();
